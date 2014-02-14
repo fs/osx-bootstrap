c79d18f32727fb33412d8d825227d45fa0e8e48c
Nickolay Abdrafikov <nicck.olay@gmail.com>
2014-02-14 16:34:32 +0400

    one more indentation fix

--- modules/functions.sh
+++ modules/functions.sh
@@ -29,7 +29,7 @@ cask_install() {
 trap catch_exit EXIT
 
 info_echo "Ask for the administrator password upfront"
-  sudo -v
+sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
