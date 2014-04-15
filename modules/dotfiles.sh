21b87fde05b23663d310a263df04e51231939605
Nickolay Abdrafikov <nickolay.abdrafikov@flatstack.com>
2014-04-15 23:31:47 +0400

    jump back to ~/.osx-bootstrap after installig dotfiles

--- modules/dotfiles.sh
+++ modules/dotfiles.sh
@@ -1,20 +1,19 @@
 info_echo "Installing bash dotfiles to ~/.dotfiles"
 
-cd ~/
-
-if [ ! -d .dotfiles ]; then
+if [ ! -d ~/.dotfiles ]; then
   git clone https://github.com/nicck/dotfiles.git ~/.dotfiles
   cd ~/.dotfiles
   sh script/bootstrap
+  cd -
 fi
 
 info_echo "Installing zsh dotfiles to ~/.dotfiles-zsh"
 
-cd ~/
-
-if [ ! -d .dotfiles-zsh ]; then
+if [ ! -d ~/.dotfiles-zsh ]; then
   mkdir ~/.dotfiles-zsh
   git clone --recurse-submodules https://github.com/fs/dotfiles.git ~/.dotfiles-zsh
   cd ~/.dotfiles-zsh
   sh install.sh
 fi
+
+cd ~/.osx-bootstrap
