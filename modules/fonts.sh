c8bff396213c0531fee8af1361393f298c0c6194
Nickolay Abdrafikov <nickolay.abdrafikov@flatstack.com>
2014-04-18 00:19:23 +0400

    add fonts module

---
+++ modules/fonts.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+if [ ! -d ~/Library/Fonts/Inconsolata-LGC ]
+then
+  # download
+  curl --output ~/Downloads/Inconsolata-LGC.zip --progress-bar \
+    "http://we.easyelectronics.ru/attachments/get/997"
+
+  # extract
+  unzip -q ~/Downloads/Inconsolata-LGC.zip \
+    -x *.ttf *.sfd *.txt \
+    -d ~/Library/Fonts
+
+  # cleanup
+  rm ~/Library/Fonts/*Inconsolata*/README
+  rm ~/Library/Fonts/*Inconsolata*/*.zip
+
+  mv ~/Library/Fonts/*Inconsolata* ~/Library/Fonts/Inconsolata-LGC
+fi
+
+# update Sublime Text 2/3 configs :: use Inconsolata LGC instead Menlo
+update_sublime_preferences() {
+  local sublime_preferences=$1
+
+  if [[ -f $sublime_preferences ]]
+  then
+    sed -e "s/Menlo/Inconsolata LGC/g" \
+        -e "s/line_padding_bottom\"\:\ 2/line_padding_bottom\"\:\ -2/g" \
+        -e "s/line_padding_top\"\:\ 1/line_padding_top\"\:\ -1/g" \
+        -e "s/font_size\"\:\ 15/font_size\"\:\ 17/g" \
+        "$sublime_preferences" > "$sublime_preferences-new"
+
+    mv -f "$sublime_preferences-new" "$sublime_preferences"
+  fi
+}
+
+update_sublime_preferences "/Users/$(whoami)/Library/Application Support/Sublime Text 2/Packages/User/Preferences.sublime-settings"
+update_sublime_preferences "/Users/$(whoami)/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
