6d61981fc909c1c624f58d0c7bc33a3aca452e4a
Nickolay Abdrafikov <nickolay.abdrafikov@flatstack.com>
2014-04-20 00:22:46 +0400

    fix workspace folders linking to ~/src

--- modules/workplace.sh
+++ modules/workplace.sh
@@ -1,6 +1,6 @@
 if ! test -e ~/src ; then
   info_echo "Setup workplace at ~/src (also linked from ~/Develop and ~/Projects"
   mkdir ~/src
-  ln -s ~/Develop ~/src
-  ln -s ~/Projects ~/src
+  ln -s ~/src ~/Develop
+  ln -s ~/src ~/Projects
 fi
