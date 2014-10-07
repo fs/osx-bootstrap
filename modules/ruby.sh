adab5c58b89d85eb6fe33764d1a7a9755d1c80ca
Dmitry Trager <dmitry.trager@flatstack.com>
2014-10-07 12:30:26 +0400

    move ruby 1.9.3 version back to script

--- modules/ruby.sh
+++ modules/ruby.sh
@@ -30,6 +30,11 @@ brew uninstall readline
 info_echo "Install readline v6.2.4 (compatible with ruby)"
 brew install https://raw.githubusercontent.com/Homebrew/homebrew/0181c8a1633353affefabe257c170edbd6d7c008/Library/Formula/readline.rb
 
+if test -z $(rbenv versions --bare|grep "1.9.3-p547"); then
+ info_echo "Install Ruby 1.9.3-p547"
+ rbenv install 1.9.3-p547
+fi
+
 if test -z $(rbenv versions --bare|grep "2.1.3"); then
   info_echo "Install Ruby 2.1.3"
   rbenv install 2.1.3
