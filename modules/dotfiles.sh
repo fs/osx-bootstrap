d41c43f1a58f43d5dd9bdacd67e14df9f60a4fa8
timurvafin <timur.vafin@flatstack.com>
2015-04-12 14:33:30 +0300

    Use fs/dotfiles

--- modules/dotfiles.sh
+++ modules/dotfiles.sh
@@ -1,7 +1,7 @@
 info_echo "Installing bash dotfiles to ~/.dotfiles"
 
 if [ ! -d ~/.dotfiles ]; then
-  git clone https://github.com/nicck/dotfiles.git ~/.dotfiles
+  git clone https://github.com/fs/dotfiles.git ~/.dotfiles
   (
     cd ~/.dotfiles
     sh script/bootstrap
