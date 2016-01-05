c7992b7df54a1a51e6439aa4a31015b761f8ee78
Marat Fakhreev <marat.fakhreev@flatstack.com>
2016-01-05 21:15:57 +0300

    Upgrade node.js to 5.5.0. Add PhantomJS npm plugin

--- modules/node.bash
+++ modules/node.bash
@@ -11,14 +11,14 @@ set +e
 source "$(brew --prefix nvm)/nvm.sh"
 set -e
 
-if test -z "$(nvm ls|grep "5.1.1")"; then
-  info_echo "Install Node.js 5.1.1"
-  nvm install 5.1.1
+if test -z "$(nvm ls|grep "5.5.0")"; then
+  info_echo "Install Node.js 5.5.0"
+  nvm install 5.5.0
 fi
 
-info_echo "Set Node.js 5.1.1 as global default Node"
-nvm use 5.1.1
-nvm alias default 5.1.1
+info_echo "Set Node.js 5.5.0 as global default Node"
+nvm use 5.5.0
+nvm alias default 5.5.0
 
 export npm_config_global=true
 export npm_config_loglevel=silent
@@ -34,3 +34,6 @@ npm install bower
 
 info_echo "Install Npm-Check-Updates"
 npm install npm-check-updates
+
+info_echo "Install PhantomJS"
+npm install phantomjs-prebuilt
