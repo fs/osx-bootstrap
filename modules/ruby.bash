b744197c091546f4c3416d20a47741c1704d8303
Timur Vafin <timur.vafin@flatstack.com>
2016-01-08 12:41:11 +0300

    Upgrade ruby to 2.2.4

--- modules/ruby.bash
+++ modules/ruby.bash
@@ -10,14 +10,14 @@ info_echo "Set default gems list"
 echo "bundler" >> "$(brew --prefix rbenv)/default-gems"
 echo "tmuxinator" >> "$(brew --prefix rbenv)/default-gems"
 
-if test -z "$(rbenv versions --bare|grep "2.2.3")"; then
-  info_echo "Install Ruby 2.2.3"
-  rbenv install 2.2.3
+if test -z "$(rbenv versions --bare|grep "2.2.4")"; then
+  info_echo "Install Ruby 2.2.4"
+  rbenv install 2.2.4
 fi
 
-info_echo "Set Ruby 2.2.3 as global default Ruby"
-rbenv global 2.2.3
-rbenv shell 2.2.3
+info_echo "Set Ruby 2.2.4 as global default Ruby"
+rbenv global 2.2.4
+rbenv shell 2.2.4
 
 info_echo "Update to latest Rubygems version"
 gem update --system --no-document
