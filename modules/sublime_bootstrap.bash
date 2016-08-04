b295f278aceed4f5e497f90e4c9551306ea041ea
Timur Vafin <timur.vafin@flatstack.com>
2016-08-04 13:32:39 +0300

    Update sublime bootstrap usage

--- modules/sublime_bootstrap.bash
+++ modules/sublime_bootstrap.bash
@@ -4,4 +4,4 @@ osx_bootstrap="$(cd "$(dirname "$0")/.." && pwd -P)"
 source "$osx_bootstrap/modules/functions.bash"
 
 info_echo "Bootstrap Sublime Text 3"
-curl -fsSL https://raw.github.com/fs/sublime-bootstrap/master/bootstrap-osx.sh | sh
+curl -fsSL https://raw.github.com/fs/sublime-bootstrap/master/bin/install | sh
