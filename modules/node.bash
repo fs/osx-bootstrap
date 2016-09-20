7a25ef60c44743915dd61fa7887a10da9d8792a5
Timur Vafin <timur.vafin@flatstack.com>
2016-09-20 11:14:03 +0300

    Load nvm.sh from $NVM_DIR

--- modules/node.bash
+++ modules/node.bash
@@ -4,12 +4,7 @@ osx_bootstrap="$(cd "$(dirname "$0")/.." && pwd -P)"
 source "$osx_bootstrap/modules/functions.bash"
 
 info_echo "Enable NVM alias"
-# we need disable -e during sourcing nvm.sh b/c of
-# https://github.com/creationix/nvm/issues/721
-# https://github.com/travis-ci/travis-ci/issues/3854#issuecomment-99492695
-set +e
-source "$(brew --prefix nvm)/nvm.sh"
-set -e
+source "$NVM_DIR/nvm.sh"
 
 if test -z "$(nvm ls|grep "node")"; then
   info_echo "Install latest Node.js version"
