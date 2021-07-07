4c9abd3f36d75f918dcf22014e6962f1b15e8b2a
Denis Zaharov <denis.zaharov@flatstack.com>
2021-07-07 13:38:50 +0300

    removed trailing whitespace

--- modules/brew.bash
+++ modules/brew.bash
@@ -15,7 +15,7 @@ fi
 info_echo "Install Brew formalue"
 
 brew tap "Homebrew/bundle" 2> /dev/null
-brew bundle install --file="$osx_bootstrap/Brewfile" 
+brew bundle install --file="$osx_bootstrap/Brewfile"
 
 info_echo "Remove outdated versions from the cellar"
 
