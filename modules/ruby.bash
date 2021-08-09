2c73506557644581d60aef4763e7765a9e37c5a3
deniszackharov <denis.zaharov@flatstack.com>
2021-08-09 11:03:28 +0300

    Fixed shellcheck errors

--- modules/ruby.bash
+++ modules/ruby.bash
@@ -4,11 +4,13 @@ set -e
 
 osx_bootstrap="$(cd "$(dirname "$0")/.." && pwd -P)"
 source "$osx_bootstrap/modules/functions.bash"
-  
-if ! command -v rbenv &> /dev/null ]
+
+if ! command -v rbenv &> /dev/null
 then
   echo 'Installing rbenv'
+  # shellcheck disable=SC2016
   [ -f "$HOME/.zprofile" ] && echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+  # shellcheck disable=SC2016
   [ -f "$HOME/.zshrc" ] && echo 'eval "$(rbenv init -)"' >> ~/.zshrc
   brew install rbenv
   brew upgrade rbenv ruby-build
